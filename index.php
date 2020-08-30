b10f55ef098afaa1a2eb412f757bf7f41258b90e
Jernej Korosec <info@phl.si>
2020-08-30 23:04:08 +0200

    backup commit

--- index.php
+++ index.php
@@ -1,39 +1,54 @@
 <?php
-
 require('lib/database.php');
 require('lib/dal.php');
 require('lib/functions.php');
-
-
-//echo "SECRestFull Security Framework";
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 
-$currDir = getcwd();
-chdir('data');
-$currDir = getcwd();
-$files1 = scandir($currDir);
+$s = new settings();
 
+$s->set_zip_dir('data')->getDirFiles()->returnFileArr();
+
+//var_dump($s,true);
+
+
+//echo $currDir = getcwd();
+/*  chdir('data');
+$dataDir = getcwd();
+$files1 = scandir($dataDir);
+*/
+/*
 remove_arr_value($files1,".");
 remove_arr_value($files1,"..");
-
+*/
 // Reindex the array
-$files1  = array_values($files1);
+//$files1  = array_values($files1);
 
-
-$file = $files1[0];
+$file = $s->fileArray[0];
+//$file = $files1[0];
 $zip = zip_open($file);
 
 if (is_resource($zip)) 
 {
     //var_dump($zip);
     $zipentry = zip_read($zip);
-    var_dump($zipentry);
+    //var_dump($zipentry);
+    $size = 0;
+    $size += zip_entry_filesize($zipentry);
+    $filename = basename(zip_entry_name($zipentry));
+    $size = $size/1024;
+    $size = $size/1024;
+    $velikost = $nombre_format_francais = number_format($size, 2, '.', ',');
+    echo $filename;
+    echo "\n";
+    echo $velikost;
+    echo "MB";
+    echo "\n";
 
-    echo $file = basename(zip_entry_name($zipentry));
     zip_close($zip);
 };
+
 
 ?>
--- lib/functions.php
+++ lib/functions.php
@@ -1,12 +1,62 @@
 
 <?php
-function remove_arr_value(&$arr, $value){ 
-    if (in_array($value, $arr)) {   
-        $key = array_search($value, $arr);
-        $key=$key+0;    
-        if($key !== false){ 
-            unset($arr[$key]);
+
+
+
+class settings{
+    public $workingPath;
+    public $currentPath;
+    public $fileArray = array();
+
+    function remove_arr_value(&$arr, $value){ 
+        if (in_array($value, $arr)) {   
+            $key = array_search($value, $arr);
+            $key=$key+0;    
+            if($key !== false){ 
+                unset($arr[$key]);
+            }
         }
+        return $this;
     }
+
+    function rem_arr_value($value){ 
+        if (in_array($value, $this->fileArray)) {   
+            $key = array_search($value, $this->fileArray);
+            $key=$key+0;    
+            if($key !== false){ 
+                unset($this->fileArray[$key]);
+            }
+        }
+        return $this;
+    }
+
+    function set_zip_dir($zipDir)
+    {
+        $this->currentPath = getcwd();
+        chdir($zipDir);
+        $this->workingPath = getcwd();
+        $this->fileArray = scandir($this->workingPath);
+        $this->rem_arr_value(".");
+        $this->rem_arr_value("..");
+        $this->fileArray  = array_values($this->fileArray);
+        chdir("..");
+        return $this;
+    }
+
+    function getDirFiles(){
+        if(!empty($workingPath))
+        $this->fileArray = scandir($workingPath);
+        return $this;
+    }
+
+    function returnFileArr(){
+        return $this->fileArray;
+    }
+
+
+
 }
+
+
+
 ?>
