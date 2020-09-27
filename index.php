8d4519076d3794d59ce1b3114ca88bd26c0df5d8
Jernej Korosec <jernejk.alfa@gmail.com>
2020-09-27 20:32:29 +0200

    TODO:Uporabi raje ZipArchive kot Kaj drugega

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ require('lib/webparser.php');
 // header("Content-Type: application/json; charset=UTF-8");
 
 $s = new settings();
+/* set working dir to data -> get files in that directory -> return those files as array */
 $s->set_zip_dir('data')->getDirFiles()->returnFileArr();
 
 //var_dump($s);
@@ -39,32 +40,63 @@ if (count($s->fileArray) > 0){
 
     foreach ($s->fileArray as $key => $file) {
 
+        echo $file;
+        echo " Contains ";
         //$file = $s->fileArray[0];
         $zip = zip_open($file);
+
+        //var_dump($zip);
+
         if (is_resource($zip)) 
         {
             //var_dump($zip);     // resource(3) of type (Zip Directory) 
-            $zipentry = zip_read($zip);
-            //var_dump($zipentry);    //resource(4) of type (Zip Entry)
-            $size = 0;
-            $size += zip_entry_filesize($zipentry);
-            //$filename_full = zip_entry_name($zipentry);
-            $filename = basename(zip_entry_name($zipentry));
-            $filename_full = getcwd() . "/" . $filename;
-            $size = $size/(1024*1024); // Koliko MB
-            $velikost = number_format($size, 2, '.', ',');
-
+            //$zipentry = zip_read($zip);
             /*
-            echo $filename;
-            echo $br;
+            echo "<PRE>";
+            var_dump($zip);
+            echo "</PRE>";
             */
-            echo $filename;
-            echo " - ";
-            echo $velikost;
-            echo "MB";
-            echo $br;
-            zip_close($zip);
+          
 
+            while ($zipentry = zip_read($zip)) {
+
+                //$zipentry2 = zip_entry_read($zipentry);
+                /*
+                echo "<PRE>";
+                var_dump($zip);
+                echo "</PRE>";
+
+                echo "<PRE>";
+                var_dump($zipentry);
+                echo "</PRE>";
+                */
+
+                /*
+                echo "<PRE>";
+                var_dump($zipentry2);
+                echo "</PRE>";
+                */
+
+                //echo  zip_entry_name($file).PHP_EOL;
+                //var_dump($zipentry);    //resource(4) of type (Zip Entry)
+                $size = 0;
+                $size += zip_entry_filesize($zipentry);
+                //$filename_full = zip_entry_name($zipentry);
+                $filename = basename(zip_entry_name($zipentry));
+                $filename_full = getcwd() . "/" . $filename;
+                $size = $size/(1024*1024); // Koliko MB
+                $velikost = number_format($size, 2, '.', ',');
+                /*
+                echo $filename;
+                echo $br;
+                */
+                echo $filename;
+                echo " - ";
+                echo $velikost;
+                echo "MB";
+                echo $br;
+                zip_close($zip);
+            }
             //$zipentry = zip_read($zip);
         };
     };
