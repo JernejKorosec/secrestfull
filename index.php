6a2499f205fe024424bd1b96baa211a3893d31ab
Jernej Korosec <jernejk.alfa@gmail.com>
2020-09-27 19:20:18 +0200

    Popravek za testiranje zip datotek 2

--- index.php
+++ index.php
@@ -13,37 +13,22 @@ $s->set_zip_dir('data')->getDirFiles()->returnFileArr();
 //var_dump($s);
 //Smo v data direktoriju
 if (count($s->fileArray) > 0){
-
-
-
     // $s->fileArray
-    array_push($s->fileArray, "apple", "raspberry","blabla.dot","blabla123.gnom","blabla123.gzip","blabla11233.zip");
-    /*
-    echo "<PRE>";
-    var_dump($s->fileArray);
-    echo "</PRE>";
-    */
+    //array_push($s->fileArray, "apple", "raspberry","blabla.dot","blabla123.gnom","blabla123.gzip","blabla11233.zip");
     $extension = ".zip";
-    $br = "<br/>";
+    //$br = "<br/>";
+
+    /*
+     * Scisti array od datotek ki niso zip datoteke
+     */
     foreach ($s->fileArray as $key => $value) {
-        //$is_in_array = in_array(array_map("strtolower", $value), array_map("strtolower", $extension));
-        //$is_in_array = in_array($value, array_map("strtolower", $extension));
-        //$is_in_array = in_array($value, "apple");
-        
+       
         $lowerValue = strtolower($s->fileArray[$key]);
         $pos = strpos($lowerValue, $extension);
         
-        //$is_zip_ext = ($pos !== FALSE) ? true : false; // ENAKO DELA
-        //$is_zip_ext = ($pos > -1) ? true : false; // DELA
-        
         $is_zip_ext = ($pos > -1) ? 1 : 0;
-        echo "{$key} => {$value} [$is_zip_ext]\n<br/>";
+        //echo "{$key} => {$value} [$is_zip_ext]\n<br/>";
 
-        //echo "{$key} => {$value} [$is_in_array]\n<br/>";
-        //print_r($s->fileArray);
-        //if(!in_array($value,$extension)) {  
-
-        //if (!in_array(array_map("strtolower", $value), array_map("strtolower", $extension))) {
         if ($is_zip_ext===0) {
             unset($s->fileArray[$key]);
         };
