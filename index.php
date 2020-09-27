f483c559eb5f5df4b3e661fbfb37373cd0853786
Jernej Korosec <jernejk.alfa@gmail.com>
2020-09-27 19:37:07 +0200

    Popravek za testiranje zip datotek 4

--- index.php
+++ index.php
@@ -37,34 +37,39 @@ if (count($s->fileArray) > 0){
     echo "</PRE>";
     */
 
-    $file = $s->fileArray[0];
-    $zip = zip_open($file);
-    if (is_resource($zip)) 
-    {
-        //var_dump($zip);     // resource(3) of type (Zip Directory) 
-        $zipentry = zip_read($zip);
-        //var_dump($zipentry);    //resource(4) of type (Zip Entry)
-        $size = 0;
-        $size += zip_entry_filesize($zipentry);
-        //$filename_full = zip_entry_name($zipentry);
-        $filename = basename(zip_entry_name($zipentry));
-        $filename_full = getcwd() . "/" . $filename;
-        $size = $size/(1024*1024); // Koliko MB
-        $velikost = number_format($size, 2, '.', ',');
+    foreach ($s->fileArray as $key => $file) {
 
-        /*
-        echo $filename;
-        echo $br;
-        */
-        echo $filename;
-        echo $br;
-        echo $velikost;
-        echo "MB";
-        echo $br;
-        zip_close($zip);
+        //$file = $s->fileArray[0];
+        $zip = zip_open($file);
+        if (is_resource($zip)) 
+        {
+            //var_dump($zip);     // resource(3) of type (Zip Directory) 
+            $zipentry = zip_read($zip);
+            //var_dump($zipentry);    //resource(4) of type (Zip Entry)
+            $size = 0;
+            $size += zip_entry_filesize($zipentry);
+            //$filename_full = zip_entry_name($zipentry);
+            $filename = basename(zip_entry_name($zipentry));
+            $filename_full = getcwd() . "/" . $filename;
+            $size = $size/(1024*1024); // Koliko MB
+            $velikost = number_format($size, 2, '.', ',');
 
-        //$zipentry = zip_read($zip);
+            /*
+            echo $filename;
+            echo $br;
+            */
+            echo $filename;
+            echo " - ";
+            echo $velikost;
+            echo "MB";
+            echo $br;
+            zip_close($zip);
+
+            //$zipentry = zip_read($zip);
+        };
     };
+
+
 };
 //echo $parsedUrl;
 
